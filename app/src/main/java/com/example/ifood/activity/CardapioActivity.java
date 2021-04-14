a08b7ed8b72856946e753be5c88de576eac1be33
Alex Silva <damasioesilva@gmail.com>
2021-04-13 23:18:25 -0300

    add cardápio produtos

---
+++ app/src/main/java/com/example/ifood/activity/CardapioActivity.java
@@ -0,0 +1,109 @@
+package com.example.ifood.activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.ifood.R;
+import com.example.ifood.adapter.AdapterProduto;
+import com.example.ifood.helper.ConfiguracaoFirebase;
+import com.example.ifood.model.Empresa;
+import com.example.ifood.model.Produto;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CardapioActivity extends AppCompatActivity {
+    private RecyclerView recyclerProdutosCardapio;
+    private ImageView imagemEmpresaCardapio;
+    private TextView textNomeEmpresaCardapio;
+    private Empresa empresaSelecionada;
+
+    private AdapterProduto adapterProduto;
+    private List<Produto> produtos = new ArrayList<>();
+    private DatabaseReference firebaseRef;
+    private String idEmpresa;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cardapio);
+
+        firebaseRef = ConfiguracaoFirebase.getFirebase();
+        inicializarComponentes();
+
+        //recuperar empresa selecionada
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null){
+            empresaSelecionada = (Empresa) bundle.getSerializable("empresa");
+            textNomeEmpresaCardapio.setText(empresaSelecionada.getNomeEmpresa());
+            //######arrumar idUsuario para idempresa no cadastro empresa#####
+            idEmpresa = empresaSelecionada.getIdUsuario();
+            String url = empresaSelecionada.getUrlImagem();
+            Picasso.get().load(url).into(imagemEmpresaCardapio);
+
+        }
+
+        //configuracao toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Cardápio");
+        setSupportActionBar(toolbar);
+        //cria o botao voltar na toolbar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //configura recyclerView
+        recyclerProdutosCardapio.setLayoutManager(new LinearLayoutManager(this));
+        recyclerProdutosCardapio.setHasFixedSize(true);
+        adapterProduto = new AdapterProduto(produtos, this);
+        recyclerProdutosCardapio.setAdapter(adapterProduto);
+
+        recuperarProdutos();
+
+
+
+
+    }
+
+    public void inicializarComponentes(){
+
+        recyclerProdutosCardapio = findViewById(R.id.recyclerEmpresaCardapio);
+        imagemEmpresaCardapio = findViewById(R.id.imageEmpresaCardapio);
+        textNomeEmpresaCardapio = findViewById(R.id.textNomeEmpresaCardapio);
+    }
+
+    private void recuperarProdutos(){
+
+        DatabaseReference produtosRef = firebaseRef
+                .child("produtos")
+                .child(idEmpresa);
+
+        produtosRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                produtos.clear();
+
+                for (DataSnapshot ds: snapshot.getChildren()){
+                    produtos.add( ds.getValue(Produto.class));
+                }
+                adapterProduto.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/ifood/activity/HomeActivity.java
+++ app/src/main/java/com/example/ifood/activity/HomeActivity.java
@@ -3,21 +3,43 @@ package com.example.ifood.activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 
 import com.example.ifood.R;
+import com.example.ifood.adapter.AdapterEmpresa;
+import com.example.ifood.adapter.AdapterProduto;
 import com.example.ifood.helper.ConfiguracaoFirebase;
+import com.example.ifood.listener.RecyclerItemClickListener;
+import com.example.ifood.model.Empresa;
+import com.example.ifood.model.Produto;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.miguelcatalan.materialsearchview.MaterialSearchView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
     private FirebaseAuth autenticacao;
     private MaterialSearchView searchView;
+    private RecyclerView recyclerEmpresa;
+    private List<Empresa> empresas = new ArrayList<>();
+    private DatabaseReference firebaseref;
+    private AdapterEmpresa adapterEmpresa;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +47,37 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         inicializarComponentes();
+        firebaseref = ConfiguracaoFirebase.getFirebase();
+
 
         autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Ifood");
         setSupportActionBar(toolbar);
+
+        //configura recyclerView
+        recyclerEmpresa.setLayoutManager(new LinearLayoutManager(this));
+        recyclerEmpresa.setHasFixedSize(true);
+        adapterEmpresa = new AdapterEmpresa(empresas);
+        recyclerEmpresa.setAdapter(adapterEmpresa);
+
+        recuperarEmpresa();
+        //config serchView
+        searchView.setHint("Pesquisar restaurante");
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                pesquisarEmpresas(newText);
+                return true;
+            }
+        });
     }
 
     @Override
@@ -86,5 +133,94 @@ public class HomeActivity extends AppCompatActivity {
     public void inicializarComponentes(){
 
         searchView = findViewById(R.id.materialSearchView);
+        recyclerEmpresa = findViewById(R.id.recyclerEmpresas);
+
+    }
+
+    public void pesquisarEmpresas(String pesquisa){
+
+        DatabaseReference empresasRef = firebaseref
+                .child("empresas");
+        Query query = empresasRef.orderByChild("nomeEmpresa")
+                .startAt(pesquisa)
+                .endAt(pesquisa + "/ufoff");
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                empresas.clear();
+
+                for (DataSnapshot ds: snapshot.getChildren()){
+                    empresas.add( ds.getValue(Empresa.class));
+                }
+                adapterEmpresa.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        clickarEmpresa();
+
+
+    }
+
+
+
+
+
+    public void recuperarEmpresa(){
+        DatabaseReference produtosRef = firebaseref
+                .child("empresas");
+
+
+        produtosRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                empresas.clear();
+
+                for (DataSnapshot ds: snapshot.getChildren()){
+                    empresas.add( ds.getValue(Empresa.class));
+                }
+                adapterEmpresa.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        clickarEmpresa();
+
+    }
+
+    public void clickarEmpresa(){
+
+        //configurar click
+        recyclerEmpresa.addOnItemTouchListener(new RecyclerItemClickListener(this,
+                recyclerEmpresa, new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Empresa empresaSelecionada = empresas.get(position);
+                Intent i = new Intent(HomeActivity.this, CardapioActivity.class);
+                i.putExtra("empresa", empresaSelecionada);
+                startActivity(i);
+
+            }
+
+            @Override
+            public void onLongItemClick(View view, int position) {
+
+            }
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        }));
+
+
     }
 }
